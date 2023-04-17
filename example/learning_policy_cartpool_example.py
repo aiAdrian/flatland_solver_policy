79f838c8462d2e7861c91be71afe2018c0fe4e13
aiAdrian <3dhelp@gmail.com>
2023-04-17 11:45:41 +0200

    cart pool analytical solver

--- example/learning_policy_cartpool_example.py
+++ example/learning_policy_cartpool_example.py
@@ -7,6 +7,20 @@ from solver.base_renderer import BaseRenderer
 from solver.base_solver import BaseSolver
 
 
+class CartPoolAnalyticalPolicy(Policy):
+    def get_name(self) -> str:
+        return self.__class__.__name__
+
+    def act(self, handle: int, state, eps=0.):
+        t, w = state[2:4]
+        if abs(t) < 0.03:
+            return 0 if w < 0 else 1
+        return 0 if t < 0 else 1
+
+    def step(self, handle: int, state, action, reward, next_state, done):
+        pass
+
+
 class CartPoolRenderer(BaseRenderer):
     def __init__(self, environment):
         super(CartPoolRenderer, self).__int__()
@@ -25,11 +39,11 @@ def create_environment():
 
 def create_dddqn_policy(observation_space: int, action_space: int) -> Policy:
     param = DDDQN_Param(hidden_size=128,
-                        buffer_size=10_000,
-                        batch_size=1024,
+                        buffer_size=5_000,
+                        batch_size=512,
                         update_every=10,
                         learning_rate=0.5e-3,
-                        tau=1.e-3,
+                        tau=1.e-2,
                         gamma=0.95,
                         buffer_min_size=0,
                         use_gpu=False)
@@ -39,6 +53,10 @@ def create_dddqn_policy(observation_space: int, action_space: int) -> Policy:
 
 def create_ppo_policy(observation_space: int, action_space: int) -> Policy:
     return PPOPolicy(observation_space, action_space, True)
+
+
+def create_cart_pool_analytical_policy() -> Policy:
+    return CartPoolAnalyticalPolicy()
 
 
 class CartPoolSolver(BaseSolver):
@@ -53,8 +71,12 @@ if __name__ == "__main__":
 
     renderer = CartPoolRenderer(env)
     solver.set_renderer(renderer)
-    solver.deactivate_rendering()
 
+
+    solver.set_policy(create_cart_pool_analytical_policy())
+    solver.do_training(max_episodes=1000)
+
+    solver.deactivate_rendering()
     solver.set_policy(create_dddqn_policy(obs_space, act_space))
     solver.do_training(max_episodes=1000)
 
