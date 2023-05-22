c47c7692d582cca3b4af53fcd7a284e60415f192
adrian_egli <adrian.egli@gmail.com>
2023-05-22 12:07:36 +0200

    sleep added

--- example/ma_gym/integration.py
+++ example/ma_gym/integration.py
@@ -1,3 +1,4 @@
+import time
 from typing import List
 
 import gym
@@ -12,9 +13,15 @@ class MultiAgentGymRenderer(BaseRenderer):
     def __init__(self, environment: Environment):
         super(MultiAgentGymRenderer, self).__int__()
         self.env = environment
+        self.sleep_time = 0.0
+
+    def set_sleep_time(self, st):
+        self.sleep_time = st
 
     def render(self, episode, step, terminal):
         self.env.get_raw_env().render()
+        if self.sleep_time > 0:
+            time.sleep(self.sleep_time)
 
 
 class MultiAgentGymSolver(FlatlandSolver):
