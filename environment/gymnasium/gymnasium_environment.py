68c5d61d85f2d3ebb1e470c1caf0a7ddb8f90fdf
adrian_egli <adrian.egli@gmail.com>
2023-10-04 09:51:39 +0200

    GymnasiumEnvironment terminate signal missmatched with all others (multi-agent)

--- environment/gymnasium/gymnasium_environment.py
+++ environment/gymnasium/gymnasium_environment.py
@@ -28,8 +28,11 @@ class GymnasiumEnvironment(Environment):
         return state, {}
 
     def step(self, actions):
-        state_next, reward, terminal, info = self.raw_env.step(actions)
-        return state_next, [reward], [terminal], info
+        state_next, reward, done, info = self.raw_env.step(actions)
+        terminal = {}
+        terminal.update({0: done})
+        terminal['__all__'] = done
+        return state_next, [reward], terminal, info
 
     def get_agent_handles(self) -> List[int]:
         return [0]
